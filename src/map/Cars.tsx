e6105447648e7fe783338e6277e8f38d2194c913
Evgeniy Klimovch <kliasanik@gmail.com>
2023-04-21 10:24:22 +0300

    th html

--- src/map/Cars.tsx
+++ src/map/Cars.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
-export const Cars = () => {
+type TopCarsType={
+    topCars:Cars[]
+}
+type Cars={
+    manufacturer:string
+    model:string
+}
+export const Cars = (props:TopCarsType) => {
     return (
         <div>
-
+            {props.topCars.map((el,index)=>{
+                return(
+                    <table>
+                        <tr>
+                            <th>{index+1}</th>
+                        </tr>
+                        <tr>
+                            <td>{el.manufacturer}</td>
+                        </tr>
+                        <tr>
+                            <td>{el.model}</td>
+                        </tr>
+                    </table>
+                )
+            })}
         </div>
     );
 };
