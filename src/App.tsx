49694c50899f9ff28b28e8040379700d9c9b4453
Evgeniy Klimovch <kliasanik@gmail.com>
2023-04-21 09:40:59 +0300

    Body and Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {Header} from "./site/Header";
+import {Body} from "./site/Body";
 
 function App() {
     return (
         <div className="App">
             <Header/>
+            <Body/>
         </div>
     );
 }
